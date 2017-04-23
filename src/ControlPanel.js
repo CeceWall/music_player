a8fbd13df4dd79a23eb9995f0407145ac501fca3
WJ <wudizhanche1000@gmail.com>
2017-04-23 12:13:51 +0800

    Almost finish play function

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -12,32 +12,43 @@ import "./ControlPanel.css";
 /**
  * @return {string}
  */
-function MillisToMinutes(milliSeconds) {
+function secondsToMinutes(seconds) {
     let date = new Date(0);
-    date.setMilliseconds(milliSeconds);
+    date.setSeconds(seconds);
     return date.toISOString().substr(14, 5)
 }
 class ControlPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChangeVolume = this.handleChangeVolume.bind(this);
+    }
+
+    handleChangeVolume(volume) {
+        this.props.onChangeVolume(volume);
+    }
 
     render() {
         const volume = this.props.volume;
-        const length = this.props.length;
+        const duration = this.props.duration;
         const now = this.props.now;
+        const paused = this.props.paused;
         return (
             <div>
                 <div id="time-and-volume">
-                    <span id="timeLength">-{MillisToMinutes(length)}</span>
+                    <span id="timeLength">-{secondsToMinutes(duration)}</span>
                     <i id="volume" className="fa fa-volume-up" aria-hidden="true">
-                        <Slider handle={() => <div></div>} className="volumeSlider" min={0} max={100}/>
+                        <Slider value={volume} handle={() => <div></div>} className="volumeSlider"
+                                onChange={this.handleChangeVolume} min={0} max={100}/>
                     </i>
                 </div>
                 <Line className="play-progress" strokeLinecap="square" strokeColor={"#6BBD7A"}
-                      percent={Math.round(now / length * 100)}/>
+                      percent={Math.round(now / duration * 100)}/>
                 <div>
                     <i className="fa fa-heart" aria-hidden="true"></i>
                     <i className="fa fa-trash" aria-hidden="true"></i>
-                    <i className="fa fa-play" aria-hidden="true"></i>
-                    <i className="fa fa-step-forward" aria-hidden="true"></i>
+                    <i className={"fa " + (paused ? "fa-play" : "fa-pause")} onClick={this.props.onPlayAndPause}
+                       aria-hidden="true"></i>
+                    <i onClick={this.props.onNextMusic} className="fa fa-step-forward" aria-hidden="true"></i>
                 </div>
             </div>
         )
--- src/MusicPlayer.js
+++ src/MusicPlayer.js
@@ -3,37 +3,100 @@
  */
 import React from 'react';
 import ControlPanel from "./ControlPanel";
+import 'whatwg-fetch'
 import ShareComponent from "./ShareComponent";
+import DocumentTitle from 'react-document-title';
 import "./MusicPlayer.css";
 class MusicPlayer extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            music: {
-                title: "Baby",
-                artist: "Justin Bieber",
-                length: 216000,
-                type: 'mp3',
-                now: 21054,
-            },
+            paused: false,
+            now: 0,
             volume: 50,
+        };
+        this.handleNextMusic = this.handleNextMusic.bind(this);
+        this.handleChangeVolume = this.handleChangeVolume.bind(this);
+        this.handlePlayAndPause = this.handlePlayAndPause.bind(this);
+        this.updateProgress = this.updateProgress.bind(this);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timeInterval);
+    }
+
+    async componentDidMount() {
+        this.timeInterval = setInterval(() => {
+            this.updateProgress();
+        }, 1000);
+        let music = await this.getMusic();
+        this.setState({
+            music: music,
+        });
+        this.audio.load();
+    }
+
+    async getMusic() {
+        let response = await fetch('http://127.0.0.1:8000/next');
+        return response.json();
+    }
+
+    updateProgress() {
+        if (this.audio && !this.audio.paused) {
+            this.setState({
+                now: this.audio.currentTime,
+            })
         }
+    }
+
+    handleChangeVolume(volume) {
+        this.setState({volume: volume});
+        this.audio.volume = volume / 100.0;
+    }
+
+    handlePlayAndPause() {
+        this.audio.paused ? this.audio.play() : this.audio.pause();
+        this.setState({paused: this.audio.paused});
+    }
+
+    async handleNextMusic() {
+        let music = await this.getMusic();
+        this.setState({
+            music: music,
+            now: 0,
+            paused: false,
+        });
+        this.audio.load();
     }
 
     render() {
         const music = this.state.music;
+        const paused = this.state.paused;
         const volume = this.state.volume;
-        return (
-            <div>
-                <audio style={{display: 'none'}}></audio>
-                <div className="title-and-artist">
-                    <h1 className="music-title">{music.title}</h1>
-                    <h2 className="music-artist">{music.artist}</h2>
+        const now = this.state.now;
+        if (music) {
+            return (
+                <div>
+                    <DocumentTitle title={music.title}/>
+                    <audio key="audio" ref={(audio) => this.audio = audio} style={{display: 'none'}} autoPlay={true}
+                           onEnded={this.handleNextMusic}>
+                        <source src={`http://127.0.0.1:8000/${music.file}`}/>
+                    </audio>
+                    <div className="title-and-artist">
+                        <h1 className="music-title">{music.title}</h1>
+                        <h2 className="music-artist">{music.artist}</h2>
+                    </div>
+                    <ControlPanel paused={paused} onChangeVolume={this.handleChangeVolume}
+                                  onNextMusic={this.handleNextMusic}
+                                  volume={volume} duration={music.duration}
+                                  now={now} onPlayAndPause={this.handlePlayAndPause}/>
+                    <ShareComponent/>
                 </div>
-                <ControlPanel volume={volume} length={music.length} now={music.now}/>
-                <ShareComponent/>
-            </div>
-        )
+            )
+        } else {
+            return <div>Coming soon.....</div>
+        }
     }
 }
 export default MusicPlayer;
